a38d3a927f5c0c5de9cc77fe0284c711de854c90
Graeme Doherty <graeme.doherty@gmail.com>
2025-12-04 15:55:20 +0000

    Initial Commit

---
+++ NextStop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextStop
+//
+//  Created by Graeme Doherty on 04/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextStop/NextStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextStopApp.swift
+//  NextStop
+//
+//  Created by Graeme Doherty on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct NextStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
